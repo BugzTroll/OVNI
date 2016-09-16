460f05c4529feae00059ff1bada06416e9b49df1
alexblouin <alex.blouin64@gmail.com>
2016-09-16 16:38:45 -0400

    LEvel Select

---
+++ Assets/OVNI Assets/Scripts/LevelSelection.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelection : MonoBehaviour {
+
+    private GameObject ui;
+	// Use this for initialization
+	void Start ()
+    {
+        ui = GameObject.Find("UI");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void onMouseDown()
+    {
+        StartOptions opt = ui.GetComponent<StartOptions>();
+        opt.sceneToStart = 2;
+        opt.Invoke("LoadDelayed", 3);
+        Destroy(gameObject);
+    }
+}
