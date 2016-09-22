f0da28c9d7bf9e948cc4fb7d4b4f49bb8c40f3c8
USHERBROOKE\gosc2103 <charles-olivier.gosselin2@usherbrooke.ca>
2016-09-22 11:22:26 -0400

    Projectile fix

--- Assets/OVNI Assets/Scripts/ProjectileShooter.cs
+++ Assets/OVNI Assets/Scripts/ProjectileShooter.cs
@@ -14,10 +14,18 @@ public class ProjectileShooter : MonoBehaviour {
 	void Update () {
         if (Input.GetMouseButtonDown(0))
         {
+        
             GameObject projectile = Instantiate(prefab) as GameObject;
-            projectile.transform.position = transform.position + Camera.main.transform.forward * 2;
+
+            // Direction of the projectile
+            Vector3 positions2 = Input.mousePosition;
+            positions2.z = 2;
+            Vector3 position = Camera.main.ScreenToWorldPoint(positions2);
+            projectile.transform.position = position;
+
+           // Velocity
             Rigidbody rb = projectile.GetComponent<Rigidbody>();
-            rb.velocity = Camera.main.transform.forward * 40;
+            rb.velocity = Camera.main.transform.forward * 10;
         }
 
 	
