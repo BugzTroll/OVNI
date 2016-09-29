5ac9e21e8ccc222499bf7a2b373f1a575e671038
USHERBROOKE\gosc2103 <charles-olivier.gosselin2@usherbrooke.ca>
2016-09-29 14:46:23 -0400

    Update live du nombre d'ammo restant

--- Assets/OVNI Assets/Scripts/GameLevelController.cs
+++ Assets/OVNI Assets/Scripts/GameLevelController.cs
@@ -3,28 +3,53 @@ using System.Collections;
 
 public class GameLevelController : MonoBehaviour {
 
+    private ProjectileShooter shooter;
+
     public UnityEngine.UI.Text scoreText;
+    public UnityEngine.UI.Text tomateText;
+    public UnityEngine.UI.Text bombText;
 
     private int score = 0;
 
 	// Use this for initialization
 	void Start () {
+
+        GameObject projectileShooterObject = GameObject.Find("Player");
+        if (projectileShooterObject != null)
+        {
+            shooter = projectileShooterObject.GetComponent<ProjectileShooter>();
+        }
+        if (shooter == null)
+        {
+            Debug.Log("Cannot find 'ProjectileShooter' script");
+        }
+
         score = 0;
-        scoreText.text = "Score: ";	
-	}
+        scoreText.text = "Score: ";
+        tomateText.text = " Tomates Restantes ";
+        bombText.text = " Bombes Restantes ";
+    }
 	
 	// Update is called once per frame
 	void Update () {
         UpdateScoreText();
+        UpdateAmmoCount();
 	}
 
     void UpdateScoreText()
     {
         scoreText.text = "Score: " + score;
     }
+    void UpdateAmmoCount()
+    {
+        tomateText.text = shooter.tomatoCount + " Tomates Restantes ";
+        bombText.text = shooter.bombCount + " Bombes Restantes ";
+    }
+
 
     public void AddScore(int addScore)
     {
         score += addScore;
     }
+
 }
