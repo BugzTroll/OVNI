abaec8f85ba22f1851abfcfae8f6305068e6063a
BugzTroll <mariedube2@gmail.com>
2016-11-29 13:54:36 -0500

    Cow Planet :D (j'ai aussi ajouté trois nouvelles ressources : KY_effects pour les effets du projectile et skyFXPack pour l'effet de lumière sous le flying disk et Flying disk qui est le mesh 3D)

---
+++ Assets/Resources/Sky FX Pack/Scripts/ScrollBehaviour.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class ScrollBehaviour : MonoBehaviour
+{
+    public int materialIndex = 0;
+    public Vector2 uvAnimationRate = new Vector2(1.0f, 0.0f);
+    public string textureName = "panelhaze";
+
+    Vector2 uvOffset = Vector2.zero;
+
+    void LateUpdate()
+    {
+        uvOffset += (uvAnimationRate * Time.deltaTime);
+        if (GetComponent<Renderer>().enabled)
+        {
+            //var temp = GetComponent<Renderer>();
+            //temp.materials[materialIndex].SetTextureOffset("MainTexture", uvOffset);
+            // GetComponent<Renderer>().materials[materialIndex].SetTextureOffset(textureName, uvOffset);
+        }
+    }
+}
