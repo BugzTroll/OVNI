25388709df995156ae4739d01eb398084f3c62a0
alexblouin <alex.blouin64@gmail.com>
2016-12-04 17:56:44 -0500

    Added trail to golf ball :D

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,14 +4,22 @@ using System.Collections;
 public class Projectile : MonoBehaviour {
 
     public bool destroyOnFirstHit;
+    private TrailRenderer trail;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        trail = GetComponent<TrailRenderer>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+	    
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.relativeVelocity.magnitude > 30.0f)
+            trail.Clear();
+        //trail.enabled = false;
+    }
 }
